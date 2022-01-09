5c0317ffe232ebbd0af3f23c5c8c7fec78110caa
hotmandu <lmn1349@gmail.com>
2022-01-10 01:56:42 +0900

    Implement Lexer

--- CodeRunner/problang/Lexer.fs
+++ CodeRunner/problang/Lexer.fs
@@ -1,4 +1,5 @@
 ﻿module Problang.Lexer
+
 open System.Text.RegularExpressions
 
 type Token =
@@ -10,63 +11,110 @@ type Token =
     | Whitespace of int
     | Comment
 
-    | Record of string
     | String of string
-
+    | Record of string
+    
 let rec private advanceIndex (text: string) pred idx =
     if text.Length <= idx || not (pred text idx) then
         idx
     else
         advanceIndex text pred (idx + 1)
 
-let rgx = Regex "qfqf"
+let rec private advanceByRegex (text: string) regex =
+    let m = Regex("^" + regex).Match(text)
+    if m.Success
+    then Some (List.tail [ for x in m.Groups -> x.Value ])
+    else None
 
-let rec public Lex (content: string) : Token seq =
-    seq {
-        if content = "" then
-            yield EOF
-            yield! Lex content
-        elif content.[0] = '\n' then
-            yield Newline
-            yield! Lex(content.Substring 1)
-        elif content.[0] = '\r' then
-            if content.Length > 2 && content.[1] = '\n' then
-                yield Newline
-                yield! Lex(content.Substring 2)
+// Comsume builder
+type ConsumeBuilder() =
+    member this.Zero() = None
+    member this.Return(x) = Some x
+
+let private consume = new ConsumeBuilder()
+
+let private consumeEOF (text: string) =
+    consume { if text = "" then return EOF, text }
+
+let private consumeNewline (text: string) =
+    consume {
+        if text.[0] = '\n' then
+            return Newline, (text.Substring 1)
+        elif text.[0] = '\r' then
+            if text.Length > 2 && text.[1] = '\n' then
+                return Newline, (text.Substring 2)
             else
-                yield Newline
-                yield! Lex(content.Substring 1)
-        elif content.[0] = ' ' || content.[0] = '\t' then
-            let wc =
-                advanceIndex content (fun x i -> x.[i] = ' ' || x.[i] = '\t') 0
-
-            yield Whitespace wc
-            yield! Lex(content.Substring wc)
-        elif content.Length > 2
-             && content.[0] = '/'
-             && content.[1] = '/' then
-            let nlc =
-                advanceIndex content (fun x i -> x.[i] <> '\r' && x.[i] <> '\n') 0
-
-            yield Comment
-            yield! Lex(content.Substring nlc)
-        elif content.[0] = '"' then
-            //TODO: support escaping \"
-            let qc =
-                (advanceIndex content (fun x i -> x.[i] <> '"') 1)
-                + 1
-
-            if qc > content.Length
-               || (qc = content.Length && content.[qc - 1] <> '"') then
-                yield Bad
-                yield! Lex ""
-            else
-                yield String(content.Substring(0, qc))
-                yield! Lex(content.Substring qc)
-        else
-            let rc =
-                advanceIndex content (fun x i -> x.[i] <> ' ' && x.[i] <> '\t' && x.[i] <> '\r' && x.[i] <> '\n') 1
-
-            yield Record(content.Substring(0, rc))
-            yield! Lex(content.Substring rc)
+                return Newline, (text.Substring 1)
     }
+
+let private consumeWhitespace (text: string) =
+    consume {
+        if text.[0] = ' ' || text.[0] = '\t' then
+            let wc =
+                advanceIndex text (fun x i -> x.[i] = ' ' || x.[i] = '\t') 0
+
+            return Whitespace wc, (text.Substring wc)
+    }
+
+let private consumeComment (text: string) =
+    consume {
+        if text.Length > 2 && text.StartsWith("//") then
+            let nlc =
+                advanceIndex text (fun x i -> x.[i] <> '\r' && x.[i] <> '\n') 0
+
+            return Comment, (text.Substring nlc)
+    }
+
+let private consumeString (text: string) =
+    consume {
+        if text.[0] = '"' then
+            match advanceByRegex text "\"((?:[^\"\\\\]|\\\\[\\s\\S])*)\"" with
+            | Some(v) -> return String(v.[0]), (text.Substring (2 + v.[0].Length))
+            | None -> return Bad, ""
+    }
+
+let private consumeRecord (text: string) =
+    consume {
+        let rc =
+            advanceIndex
+                text
+                (fun x i ->
+                    x.[i] <> ' '
+                    && x.[i] <> '\t'
+                    && x.[i] <> '\r'
+                    && x.[i] <> '\n')
+                1
+
+        return Record(text.Substring(0, rc)), (text.Substring rc)
+    }
+
+let private consumerTable: (string -> (Token * string) option) list =
+    [ consumeEOF
+      consumeNewline
+      consumeWhitespace
+      consumeComment
+
+      consumeString
+      consumeRecord ]
+
+let rec private tryUntilSome (funclist: ('a -> 'b option) list) (arg: 'a) =
+    match funclist with
+    | hd :: tl ->
+        match (hd arg) with
+        | None -> tryUntilSome tl arg
+        | v -> v
+    | [] -> None
+
+
+/// <summary>
+/// Lexical Analyser, converts input string into tokens.
+/// </summary>
+/// <param name="content">An input string to tokenize</param>
+let rec public Lex (content: string) : Token seq =
+    match (tryUntilSome consumerTable content) with
+    | Some (token, rest) ->
+        seq {
+            yield token
+            yield! Lex rest
+        }
+    | None -> failwith "Lex Consuming Failure"
